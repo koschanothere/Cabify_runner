1e80fa86b43a3f7f8f926fe9bef231733d0adf2f
koschanothere <chistyakost@gmail.com>
2024-05-19 12:17:40 +0300

    tester added

---
+++ Analytics/tester.py
@@ -0,0 +1,7 @@
+import logging
+
+
+logging.basicConfig(filename='scraper.logs', level=logging.INFO, 
+                    format='%(asctime)s %(levelname)s:%(message)s', encoding='utf-8')
+
+logging.info("PLEASE WORK")
